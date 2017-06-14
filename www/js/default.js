7ded26ebdfcb6b9ac9878653c62ae0d1af2a7521
mimopenguin <mimopenguin@gmail.com>
2017-06-14 19:55:27 +0800

    Add files via upload

---
+++ www/js/default.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+function account(){
+	if(document.getElementsByClassName("id").value==null||document.getElementsByClassName("pwd").value==null){
+		
+		alert("enter the right id password");
+	}
+	else{
+		alert("Success");
+	}
+	
+}
