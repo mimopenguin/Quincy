db82550370b30787ea61735d02972deab11c3e41
mimopenguin <mimopenguin@gmail.com>
2017-06-15 22:17:06 +0800

    Add files via upload

---
+++ www/js/default.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="../default.html" />
+// 有关“空白”模板的简介，请参阅以下文档:
+// http://go.microsoft.com/fwlink/?LinkID=392286
+(function () {
+    "use strict";
+
+    var app = WinJS.Application;
+    var activation = Windows.ApplicationModel.Activation;
+
+    app.onactivated = function (args) {
+        if (args.detail.kind === activation.ActivationKind.launch) {
+            if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+                // TODO: 此应用程序刚刚启动。在此处初始化
+                //您的应用程序。
+            } else {
+                // TODO: 此应用程序已挂起，然后终止。
+                // 若要创造顺畅的用户体验，请在此处还原应用程序状态，使应用似乎永不停止运行。
+            }
+            args.setPromise(WinJS.UI.processAll());
+        }
+    };
+
+    app.oncheckpoint = function (args) {
+        // TODO: 即将挂起此应用程序。在此处保存
+        //需要在挂起中保留的任何状态。您可以使用
+        // WinJS.Application.sessionState 对象，该对象将在
+        //挂起中自动保存和恢复。如果您需要在
+        //挂起应用程序之前完成异步操作，请调用
+        // args.setPromise()。
+    };
+
+    app.start();
+})();
---
+++ www/js/homepage.js
@@ -0,0 +1,15 @@
+﻿function homeclick() {
+    window.location.href = "default.html";
+}
+function searchclick() {
+    window.location.href = "search.html";
+}
+function recclick() {
+    window.location.href = "sale.html";
+}
+function cartclick(){
+    window.location.href = "cart.html";
+}
+function search(){
+	window.location.href="search.html";
+}
